eda0f4d0be001799d4d79fffdf83b32fb27c3aca
Fred Ma <fredma1234@gmail.com>
2021-01-30 18:47:26 -0500

    Got logging working with Log4j2 Added spring-boot-starter-log4j2 dependency Added log4j2.xml

--- spring-demo-annotations/src/main/scala/com/luv2code/springdemo/AnnotationBeansScopeDemoApp.scala
+++ spring-demo-annotations/src/main/scala/com/luv2code/springdemo/AnnotationBeansScopeDemoApp.scala
@@ -1,10 +1,18 @@
 package com.luv2code.springdemo
 
 import com.luv2code.springdemo.config.AppConfig
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.config.Configurator
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 object AnnotationBeansScopeDemoApp {
+  val logger: Logger = LogManager.getLogger(classOf[AnnotationBeansScopeDemoApp])
+
+  Configurator.initialize(null, "D:\\Documents\\IdeaProjects\\spring-hibernate-udemy2\\spring-demo-annotations\\src\\main\\resources\\log4j2.xml")
+
   def main(args: Array[String]): Unit = {
+    logger.info("test")
     val appContext = new AnnotationConfigApplicationContext(classOf[AppConfig])
     val theCoach = appContext.getBean("tennisCoach", classOf[Coach])
     val alphaCoach = appContext.getBean("tennisCoach", classOf[Coach])
@@ -14,3 +22,6 @@ object AnnotationBeansScopeDemoApp {
     appContext.close()
   }
 }
+
+class AnnotationBeansScopeDemoApp  {}
+
