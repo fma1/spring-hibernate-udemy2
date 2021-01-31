7b4e03651e32b3a8263f8fefbe344c97ef42707b
Fred Ma <fredma1234@gmail.com>
2021-01-31 16:45:47 -0500

    Commented out component scanning since @Bean annotation is supposed to work without it

--- spring-demo-annotations/src/main/scala/com/luv2code/springdemo/config/AppConfig.scala
+++ spring-demo-annotations/src/main/scala/com/luv2code/springdemo/config/AppConfig.scala
@@ -5,7 +5,7 @@ import com.luv2code.springdemo.service.{FortuneService, HappyFortuneService}
 import org.springframework.context.annotation.{Bean, ComponentScan, Configuration, PropertySource}
 
 @Configuration
-@ComponentScan(basePackages = Array("com.luv2code.springdemo"))
+// @ComponentScan(basePackages = Array("com.luv2code.springdemo"))
 @PropertySource(Array("classpath:sport.properties"))
 class AppConfig {
   @Bean
