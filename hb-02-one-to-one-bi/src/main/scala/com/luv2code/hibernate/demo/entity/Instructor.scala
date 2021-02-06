ac3bc9c8015a7c98e0af27177ab0068a7873043a
Fred Ma <fredma1234@gmail.com>
2021-02-06 15:09:20 -0500

    Fixed Instructor & InstructorDetail toString() since the global toString() was calling each other recursively

---
+++ hb-02-one-to-one-bi/src/main/scala/com/luv2code/hibernate/demo/entity/Instructor.scala
@@ -0,0 +1,50 @@
+package com.luv2code.hibernate.demo.entity
+
+import org.hibernatewrapper.util.HBUtil
+
+import javax.persistence._
+import scala.beans.BeanProperty
+
+@Entity
+@Table(name = "instructor")
+class Instructor(_firstName: String, _lastName: String, _email: String) {
+  @Id
+  @BeanProperty
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  var id: Int = _
+
+  @BeanProperty
+  @Column(name = "first_name")
+  var firstName: String = _firstName
+
+  @BeanProperty
+  @Column(name = "last_name")
+  var lastName: String = _lastName
+
+  @BeanProperty
+  @Column(name = "email")
+  var email: String = _email
+
+  @BeanProperty
+  @OneToOne(cascade = Array(CascadeType.ALL))
+  @JoinColumn(name = "instructor_detail_id")
+  var instructorDetail: InstructorDetail = _
+
+  private def this() = this(null, null, null)
+
+  override def toString: String = {
+    val builder = new StringBuilder
+    builder.append("Instructor(")
+    builder.append(s"id: $id, ")
+    builder.append(s"firstName: $firstName, ")
+    builder.append(s"lastName: $lastName, ")
+    builder.append(s"email: $email)")
+    builder.toString()
+  }
+}
+
+object Instructor {
+  def apply(_firstName: String, _lastName: String, _email: String) =
+    new Instructor(_firstName, _lastName, _email)
+}
