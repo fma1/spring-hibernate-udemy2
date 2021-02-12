422e009153c82263e5a16b0b787257cf37188589
Fred Ma <fredma1234@gmail.com>
2021-02-12 14:38:16 -0500

    Added service layer CustomerService and modified CustomerController to use the service layer rather than DAO layer Modified CustomerDAO to no longer use @Transactional annotation

--- web-customer-tracker/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
+++ web-customer-tracker/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.controller
 
 import com.luv2code.springdemo.dao.CustomerDAO
+import com.luv2code.springdemo.service.CustomerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{GetMapping, RequestMapping}
@@ -11,11 +12,11 @@ import org.springframework.ui.Model
 @RequestMapping(Array("/customer"))
 class CustomerController {
   @Autowired
-  private var customerDAO: CustomerDAO = _
+  private var customerService: CustomerService = _
 
   @GetMapping(Array("/list"))
   def listCustomers(model: Model): String = {
-    model.addAttribute("customers", customerDAO.getCustomers)
+    model.addAttribute("customers", customerService.getCustomers)
     "list-customers"
   }
 }
--- web-customer-tracker/src/main/scala/com/luv2code/springdemo/service/CustomerService.scala
+++ web-customer-tracker/src/main/scala/com/luv2code/springdemo/service/CustomerService.scala
@@ -1,20 +1,20 @@
-package com.luv2code.springdemo.dao
+package com.luv2code.springdemo.service
 
+import com.luv2code.springdemo.dao.CustomerDAO
 import com.luv2code.springdemo.entity.Customer
-import org.hibernate.SessionFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 import java.util.{List => JList}
+import javax.transaction.Transactional
 
 @Repository
-class CustomerDAO {
+class CustomerService {
   @Autowired
-  private var sessionFactory: SessionFactory = _
+  private var customerDAO: CustomerDAO = _
 
+  @Transactional
   def getCustomers: JList[Customer] = {
-    sessionFactory.getCurrentSession
-      .createQuery("from Customer", classOf[Customer])
-      .getResultList
+    customerDAO.getCustomers
   }
 }
