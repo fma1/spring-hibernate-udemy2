b9afa51c8f7d3f4da8d7ada7bfbbe0ecd4d04bdd
Fred Ma <fredma1234@gmail.com>
2021-02-12 14:28:50 -0500

    Changed RequestMapping to GetMapping

--- web-customer-tracker/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
+++ web-customer-tracker/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
@@ -3,7 +3,7 @@ package com.luv2code.springdemo.controller
 import com.luv2code.springdemo.dao.CustomerDAO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.{GetMapping, RequestMapping}
 import org.springframework.ui.Model
 
 
@@ -13,7 +13,7 @@ class CustomerController {
   @Autowired
   private var customerDAO: CustomerDAO = _
 
-  @RequestMapping(Array("/list"))
+  @GetMapping(Array("/list"))
   def listCustomers(model: Model): String = {
     model.addAttribute("customers", customerDAO.getCustomers)
     "list-customers"
