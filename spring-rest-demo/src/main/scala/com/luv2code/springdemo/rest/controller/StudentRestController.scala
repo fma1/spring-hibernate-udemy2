968193ae6873333f4a03f564340c3c5deeff7369
Fred Ma <fredma1234@gmail.com>
2021-02-19 12:54:44 -0500

    Added @PathVariable endpoint to get a single student

--- spring-rest-demo/src/main/scala/com/luv2code/springdemo/rest/controller/StudentRestController.scala
+++ spring-rest-demo/src/main/scala/com/luv2code/springdemo/rest/controller/StudentRestController.scala
@@ -1,15 +1,20 @@
 package com.luv2code.springdemo.rest.controller
 
 import com.luv2code.springdemo.rest.entity.Student
-import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RestController}
+import org.springframework.web.bind.annotation.{GetMapping, PathVariable, RequestMapping, RestController}
+
+import scala.collection.mutable.ArrayBuffer
 
 @RestController
 @RequestMapping(Array("/api"))
 class StudentRestController {
-  @GetMapping(Array("/students"))
-  def getStudents: List[Student] = {
-    List(Student("Poornima", "Patel"),
+  val aryBuf: ArrayBuffer[Student] = ArrayBuffer(Student("Poornima", "Patel"),
       Student("Mario", "Rossi"),
       Student("Mary", "Smith"))
-  }
+
+  @GetMapping(Array("/students/{studentId}"))
+  def getStudents(@PathVariable studentId: Int): Student = aryBuf(studentId)
+
+  @GetMapping(Array("/students"))
+  def getStudents: ArrayBuffer[Student] = aryBuf
 }
