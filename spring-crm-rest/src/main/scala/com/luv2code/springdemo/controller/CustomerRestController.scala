9998c14daecac891bc181aae7162a9c720e27af3
Fred Ma <fredma1234@gmail.com>
2021-02-20 09:24:51 -0500

    Added functionality to get customers and get single customer

---
+++ spring-crm-rest/src/main/scala/com/luv2code/springdemo/controller/CustomerRestController.scala
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo.controller
+
+import com.luv2code.springdemo.entity.Customer
+import com.luv2code.springdemo.service.CustomerService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation._
+
+import java.util.{List => JList}
+
+@RestController
+@RequestMapping(Array("/customer"))
+class CustomerRestController {
+  @Autowired
+  private var customerService: CustomerService = _
+
+  @GetMapping("/customers")
+  def getCustomers: JList[Customer] = customerService.getCustomers
+
+  @GetMapping("/customers/{customerId}")
+  def getCustomer(@PathVariable customerId: Int): Customer =
+    customerService.getCustomerById(customerId)
+}
