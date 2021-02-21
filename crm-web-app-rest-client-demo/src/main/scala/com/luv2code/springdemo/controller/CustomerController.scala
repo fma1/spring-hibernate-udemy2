40e11127beb78554409ae9bb9271c66600590f2c
Fred Ma <fredma1234@gmail.com>
2021-02-21 09:03:44 -0500

    Renamed class CustomerService -> CustomerServiceImpl and added trait CustomerService to allow for another implementation

--- crm-web-app-rest-client-demo/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
+++ crm-web-app-rest-client-demo/src/main/scala/com/luv2code/springdemo/controller/CustomerController.scala
@@ -1,7 +1,7 @@
 package com.luv2code.springdemo.controller
 
 import com.luv2code.springdemo.entity.Customer
-import com.luv2code.springdemo.service.CustomerService
+import com.luv2code.springdemo.service.CustomerServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{GetMapping, ModelAttribute, PostMapping, RequestMapping, RequestParam}
@@ -12,7 +12,7 @@ import org.springframework.ui.Model
 @RequestMapping(Array("/customer"))
 class CustomerController {
   @Autowired
-  private var customerService: CustomerService = _
+  private var customerService: CustomerServiceImpl = _
 
   @GetMapping(Array("/list"))
   def listCustomers(model: Model): String = {
--- crm-web-app-rest-client-demo/src/main/scala/com/luv2code/springdemo/service/CustomerService.scala
+++ crm-web-app-rest-client-demo/src/main/scala/com/luv2code/springdemo/service/CustomerService.scala
@@ -1,35 +1,12 @@
 package com.luv2code.springdemo.service
 
-import com.luv2code.springdemo.dao.CustomerDAO
 import com.luv2code.springdemo.entity.Customer
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Repository
 
 import java.util.{List => JList}
-import javax.transaction.Transactional
 
-@Repository
-class CustomerService {
-  @Autowired
-  private var customerDAO: CustomerDAO = _
-
-  @Transactional
-  def getCustomerById(customerId: Int): Customer = {
-    customerDAO.getCustomerById(customerId)
-  }
-
-  @Transactional
-  def getCustomers: JList[Customer] = {
-    customerDAO.getCustomers
-  }
-
-  @Transactional
-  def saveCustomer(customer: Customer): Unit = {
-    customerDAO.saveCustomer(customer)
-  }
-
-  @Transactional
-  def deleteCustomer(customerId: Int): Unit = {
-    customerDAO.deleteCustomer(customerId)
-  }
+trait CustomerService {
+  def getCustomerById(customerId: Int): Customer
+  def getCustomers: JList[Customer]
+  def saveCustomer(customer: Customer): Unit
+  def deleteCustomer(customerId: Int): Unit
 }
